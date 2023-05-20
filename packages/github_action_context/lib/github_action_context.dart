6644810f46a0e79d4aaf65d16393dfd990b7dd7d
cjl_macbook <cjl_spy@163.com>
2023-05-20 21:40:24 +0800

    feat: update index_file

--- packages/github_action_context/lib/github_action_context.dart
+++ packages/github_action_context/lib/github_action_context.dart
@@ -1,8 +1,7 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library;
+// Copyright 2023 The dart-action Authors. All rights reserved.
+// Use of this source code is governed by a Apache 2.0 license that can be
+// found in the LICENSE file.
+
+library github_action_context;
 
 export 'src/github_action_context_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
