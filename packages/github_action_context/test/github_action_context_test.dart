ad700a6f0325ae8fba15d82814569d6f38b0c6c8
cjl_macbook <cjl_spy@163.com>
2023-05-20 21:29:46 +0800

    feat: init commit

---
+++ packages/github_action_context/test/github_action_context_test.dart
@@ -0,0 +1,16 @@
+import 'package:github_action_context/github_action_context.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
