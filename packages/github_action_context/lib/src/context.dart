e3ed8fdc802c125d63f095ae1dee11bc68a1c7ba
cjl_macbook <cjl_spy@163.com>
2023-05-20 22:31:55 +0800

    feat: define context

--- packages/github_action_context/lib/src/context.dart
+++ packages/github_action_context/lib/src/context.dart
@@ -1,3 +1,109 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'interface.dart';
+
 final context = Context();
 
-class Context {}
+class Context {
+  late WebhookPayload payload;
+
+  late String eventName;
+  late String sha;
+  late String ref;
+  late String workflow;
+  late String action;
+  late String actor;
+  late String job;
+  late int runNumber;
+  late int runId;
+  late String apiUrl;
+  late String serverUrl;
+  late String graphqlUrl;
+
+  Context() {
+    if (Platform.environment.containsKey('GITHUB_EVENT_PATH')) {
+      String eventPath = Platform.environment['GITHUB_EVENT_PATH']!;
+      if (File(eventPath).existsSync()) {
+        String eventJson = File(eventPath).readAsStringSync();
+        payload = WebhookPayload.fromJson(jsonDecode(eventJson));
+      } else {
+        stdout.write('GITHUB_EVENT_PATH $eventPath does not exist\n');
+      }
+    }
+
+    eventName = Platform.environment['GITHUB_EVENT_NAME']!;
+    sha = Platform.environment['GITHUB_SHA']!;
+    ref = Platform.environment['GITHUB_REF']!;
+    workflow = Platform.environment['GITHUB_WORKFLOW']!;
+    action = Platform.environment['GITHUB_ACTION']!;
+    actor = Platform.environment['GITHUB_ACTOR']!;
+    job = Platform.environment['GITHUB_JOB']!;
+    runNumber = int.parse(Platform.environment['GITHUB_RUN_NUMBER']!);
+    runId = int.parse(Platform.environment['GITHUB_RUN_ID']!);
+    apiUrl = Platform.environment['GITHUB_API_URL'] ?? 'https://api.github.com';
+    serverUrl =
+        Platform.environment['GITHUB_SERVER_URL'] ?? 'https://github.com';
+    graphqlUrl = Platform.environment['GITHUB_GRAPHQL_URL'] ??
+        'https://api.github.com/graphql';
+  }
+
+  IssueInfo get issue {
+    final payload = this.payload;
+
+    int n;
+
+    if (payload.issue != null) {
+      n = payload.issue!['number'];
+    } else if (payload.pullRequest != null) {
+      n = payload.pullRequest!['number'];
+    } else {
+      n = payload['number'];
+    }
+
+    return IssueInfo(
+      owner: repo.owner,
+      repo: repo.repo,
+      number: n,
+    );
+  }
+
+  RepoInfo get repo {
+    if (Platform.environment.containsKey('GITHUB_REPOSITORY')) {
+      final repoParts = Platform.environment['GITHUB_REPOSITORY']!.split('/');
+      return RepoInfo(owner: repoParts[0], repo: repoParts[1]);
+    }
+
+    if (payload.repository != null) {
+      return RepoInfo(
+        owner: payload.repository!['owner']['login']!,
+        repo: payload.repository!['name']!,
+      );
+    }
+
+    throw Exception(
+        "context.repo requires a GITHUB_REPOSITORY environment variable like 'owner/repo'");
+  }
+}
+
+class IssueInfo {
+  final String owner;
+  final String repo;
+  final int? number;
+
+  IssueInfo({
+    required this.owner,
+    required this.repo,
+    this.number,
+  });
+}
+
+class RepoInfo {
+  final String owner;
+  final String repo;
+
+  RepoInfo({
+    required this.owner,
+    required this.repo,
+  });
+}
--- packages/github_action_context/lib/src/interface.dart
+++ packages/github_action_context/lib/src/interface.dart
@@ -1,20 +1,55 @@
+import 'dart:collection';
 import 'dart:convert';
 
-class PayloadRepository {
+class _BaseItem with MapMixin<dynamic, dynamic> {
+  final Map innerMap;
+
+  _BaseItem(this.innerMap);
+
+  @override
+  operator [](Object? key) {
+    return innerMap[key];
+  }
+
+  @override
+  void operator []=(key, value) {
+    innerMap[key] = value;
+  }
+
+  @override
+  void clear() {
+    innerMap.clear();
+  }
+
+  @override
+  Iterable get keys => innerMap.keys;
+
+  @override
+  remove(Object? key) {
+    return innerMap.remove(key);
+  }
+}
+
+class PayloadRepository extends _BaseItem {
   String? fullName;
   String name;
   Owner owner;
   String? htmlUrl;
 
-  PayloadRepository({
+  PayloadRepository(
+    super.innerMap, {
     this.fullName,
     required this.name,
     required this.owner,
     this.htmlUrl,
   });
 
-  factory PayloadRepository.fromJson(Map json) {
+  static PayloadRepository? fromJson(Map? json) {
+    if (json == null) {
+      return null;
+    }
     return PayloadRepository(
+      json,
       fullName: json['full_name'],
       name: json['name'],
       owner: Owner.fromJson(json['owner']),
@@ -22,31 +57,29 @@ class PayloadRepository {
     );
   }
 
-  factory PayloadRepository.fromString(String text) {
-    final json = jsonDecode(text);
-    return PayloadRepository.fromJson(json);
-  }
 }
 
-class Owner {
+class Owner extends _BaseItem {
   String login;
   String? name;
 
-  Owner({
+  Owner(
+    super.innerMap, {
     required this.login,
     this.name,
   });
 
-  factory Owner.fromJson(Map<String, dynamic> json) {
+  factory Owner.fromJson(Map json) {
     return Owner(
+      json,
       login: json['login'],
       name: json['name'],
     );
   }
 }
 
-class WebhookPayload {
-  Map<String, dynamic> repository;
+class WebhookPayload extends _BaseItem {
+  PayloadRepository? repository;
   Map<String, dynamic>? issue;
   Map<String, dynamic>? pullRequest;
   Map<String, dynamic>? sender;
@@ -54,7 +87,8 @@ class WebhookPayload {
   Map<String, dynamic>? installation;
   Map<String, dynamic>? comment;
 
-  WebhookPayload({
+  WebhookPayload(
+    super.innerMap, {
     required this.repository,
     this.issue,
     this.pullRequest,
@@ -64,9 +98,10 @@ class WebhookPayload {
     this.comment,
   });
 
-  factory WebhookPayload.fromJson(Map<String, dynamic> json) {
+  factory WebhookPayload.fromJson(Map json) {
     return WebhookPayload(
-      repository: json['repository'],
+      json,
+      repository: PayloadRepository.fromJson(json['repository']),
       issue: json['issue'],
       pullRequest: json['pull_request'],
       sender: json['sender'],
@@ -74,5 +109,11 @@ class WebhookPayload {
       installation: json['installation'],
       comment: json['comment'],
     );
+  }
+
+
+  factory WebhookPayload.fromString(String text) {
+    final json = jsonDecode(text);
+    return WebhookPayload.fromJson(json);
   }
 }
